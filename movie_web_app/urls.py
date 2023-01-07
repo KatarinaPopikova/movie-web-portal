c38b09bfcfb1e7ce2380e6e5f252bda5e4dd02d8
kStasova <stasovakatka6688@gmail.com>
2023-01-07 01:52:43 +0100

    fix: implement category and popular search

--- movie_web_app/urls.py
+++ movie_web_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ListNamesToDetect, ListMoviesTmdb, MovieDetailTmdb, MovieImagesTmdb, MovieDetailImdb, \
-    MovieImagesImdb, MoviePostersImdb, PosterListMoviesTmdb
+from .views import ListCategoriesToDetect, ListPopularMoviesTmdb, ListMoviesTmdb, MovieDetailTmdb, MovieImagesTmdb, \
+    MovieDetailImdb,  MovieImagesImdb, MoviePostersImdb, PosterListMoviesTmdb
 
 urlpatterns = [
-    path('tmdb/movies/names_to_detect', ListNamesToDetect.as_view(), name='list_names_to_detect'),
+    path('categories_to_detect', ListCategoriesToDetect.as_view(), name='list_categories_to_detect'),
     path('tmdb/movies/<int:movie_id>', MovieDetailTmdb.as_view(), name='movie_detail_tmdb'),
+    path('tmdb/movies/popular', ListPopularMoviesTmdb.as_view(), name='list_popular_movies_tmdb'),
     path('tmdb/movies/searchTitle', ListMoviesTmdb.as_view(), name='list_movies_tmdb'),
     path('tmdb/movies/searchPoster', PosterListMoviesTmdb.as_view(), name='poster_list_movies_tmdb'),
     path('tmdb/movies/images/<int:movie_id>', MovieImagesTmdb.as_view(), name='movie_images_tmdb'),
