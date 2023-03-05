fc9d24dca427e2ae6e121c1fb88c561201af3965
kStasova <stasovakatka6688@gmail.com>
2023-03-05 18:08:12 +0100

    refactor: merged 3 functions into call_api_multiple_times

--- movie_web_app/urls.py
+++ movie_web_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import ListCategoriesToDetect, ListPopularMoviesTmdb, ListMoviesTmdb, MovieDetailTmdb, MovieImagesTmdb, \
+from .views import ListCategoriesToDetect, ListPopularMoviesTmdb, ListMoviesWithTitleTmdb, ListMoviesTmdb, MovieDetailTmdb, MovieImagesTmdb, \
     MovieDetailImdb,  MovieImagesImdb, MoviePostersImdb, PosterListMoviesTmdb, TrailerListMoviesTmdb
 
 urlpatterns = [
     path('categories_to_detect', ListCategoriesToDetect.as_view(), name='list_categories_to_detect'),
     path('tmdb/movies/<int:movie_id>', MovieDetailTmdb.as_view(), name='movie_detail_tmdb'),
     path('tmdb/movies/popular', ListPopularMoviesTmdb.as_view(), name='list_popular_movies_tmdb'),
-    path('tmdb/movies/searchTitle', ListMoviesTmdb.as_view(), name='list_movies_tmdb'),
+    path('tmdb/movies/searchMoviesWithTitle', ListMoviesWithTitleTmdb.as_view(), name='list_movies_with_title_tmdb'),
+    path('tmdb/movies/searchMovies', ListMoviesTmdb.as_view(), name='list_movies_tmdb'),
     path('tmdb/movies/searchPoster', PosterListMoviesTmdb.as_view(), name='poster_list_movies_tmdb'),
     path('tmdb/movies/searchTrailer', TrailerListMoviesTmdb.as_view(), name='trailer_list_movies_tmdb'),
     path('tmdb/movies/images/<int:movie_id>', MovieImagesTmdb.as_view(), name='movie_images_tmdb'),
--- movie_web_app/views.py
+++ movie_web_app/views.py
@@ -23,7 +23,7 @@ class ListCategoriesToDetect(APIView):
             response['status'] = 200
             response['message'] = 'success'
         else:
-            response['status'] = 300
+            response['status'] = 500
             response['message'] = 'error'
 
         return Response(response)
@@ -35,77 +35,52 @@ class ListPopularMoviesTmdb(APIView):
         return manage_with_external_response(external_response)
 
 
-class ListMoviesTmdb(APIView):
+class ListMoviesWithTitleTmdb(APIView):
     def get(self, request):
         external_response = requests.get(f'{TMDB_API}search/movie?api_key={API_KEY_TMDB}&query={request.GET["query"]}/')
         return manage_with_external_response(external_response)
 
 
+class ListMoviesTmdb(APIView):
+    def get(self, request):
+        external_response = f'{TMDB_API}discover/movie?api_key={API_KEY_TMDB}&with_genres={request.GET["genres"]}&release_date.gte={request.GET["date_from"]}&release_date.lte={request.GET["date_to"]}'
+        print(external_response)
+        response = call_api_multiple_times(external_response)
+        response["credentials"]["results"] = [movie for movie in response["credentials"]["results"] if
+                                              movie.get('title').lower().find(request.GET["query"].lower()) != -1]
+        return Response(response)
+
+
 class PosterListMoviesTmdb(APIView):
     def get(self, request):
-        if request.GET["query"] != "":
-            external_request = f'{TMDB_API}search/movie?api_key={API_KEY_TMDB}&query={request.GET["query"]}/'
-        else:
-            external_request = f'{TMDB_API}discover/movie?api_key={API_KEY_TMDB}&with_genres={request.GET["genres"]}'
-            f'&primary_release_date.gte={request.GET["date_from"]}&primary_release_date.lte={request.GET["date_to"]}'
 
-        return call_api_multiple_times(external_request, request.GET["categories"])
+        if request.GET["query"] != "":
+            external_response = f'{TMDB_API}search/movie?api_key={API_KEY_TMDB}&query={request.GET["query"]}/'
+        else:
+            external_response = f'{TMDB_API}discover/movie?api_key={API_KEY_TMDB}&with_genres={request.GET["genres"]}&primary_release_date.gte={request.GET["date_from"]}&primary_release_date.lte={request.GET["date_to"]}'
+
+        response = call_api_multiple_times(external_response)
+        posters_links, movie_ids = create_array_from_posters_link(response['credentials']['results'])
+        results = detect_main(posters_links, movie_ids, request.GET["categories"].split(','))
+        response['credentials'] = json.loads(results)
+        return Response(response)
+
 
 class TrailerListMoviesTmdb(APIView):
     def get(self, request):
         external_request = f'{TMDB_API}discover/movie?api_key={API_KEY_TMDB}&query={request.GET["query"]}&with_genres={request.GET["genres"]}'
         f'&primary_release_date.gte={request.GET["date_from"]}&primary_release_date.lte={request.GET["date_to"]}'
-        print("in Trailer")
-        return get_trailer_link_list(external_request)
-
-
-def get_trailer_link_list(external_request):
-    response = {}
-    data = {}
-
-    for page in range(6):
-        external_response = requests.get(f'{external_request}&page={page}')
-        external_response_status = external_response.status_code
-
-        if page == 1:
-            response['status'] = external_response_status
-            if external_response_status == 200:
-                response['credentials'] = external_response.json()
-                response['message'] = 'success'
-            else:
-                response['message'] = 'error'
-                break
-        else:
-            if external_response_status == 200:
-                data = (*data, *external_response.json()['results'])
-
-    response['credentials']['results'] = (*data, *response['credentials']['results'])
-    movie_ids = [movie['id'] for movie in response['credentials']['results']]
-    response['credentials']['results'] = create_array_from_trailer_link(movie_ids)
-    return Response(response)
-
-def create_array_from_trailer_link(movie_ids):
-    start_path = 'https://youtu.be/'
-    trailer_dict = {}
-    for movie_id in movie_ids:
-        video_response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={API_KEY_TMDB}')
-        print(video_response.json()["results"])
-        trailer_list = {}
-        for video in video_response.json()["results"]:
-            if video["site"] == "YouTube":
-                trailer_list.setdefault("link",start_path + video["key"])
-        if len(trailer_list) > 0:
-            #tie iste id mi prepisuje
-            trailer_dict["id"] = movie_id
-            trailer_dict["trailer"] = trailer_list
-
-    return trailer_dict
+        response = call_api_multiple_times(external_request)
+        movie_ids = [movie['id'] for movie in response['credentials']['results']]
+        response['credentials']['results'] = create_array_from_trailer_link(movie_ids)
+        return Response(response)
 
 
 class MovieDetailTmdb(APIView):
 
     def get(self, request, movie_id):
-        external_response = requests.get(f'{TMDB_API}movie/{movie_id}?api_key={API_KEY_TMDB}&append_to_response=credits')
+        external_response = requests.get(
+            f'{TMDB_API}movie/{movie_id}?api_key={API_KEY_TMDB}&append_to_response=credits')
         return manage_with_external_response(external_response)
 
 
@@ -156,15 +131,18 @@ def manage_with_external_response(external_response):
     return Response(response)
 
 
-def call_api_multiple_times(external_request, categories):
+def call_api_multiple_times(external_request, max_pages=100):
     response = {}
     data = {}
+    total_pages = 1
+    actual_page = 1
 
-    for page in range(1, 21):
-        external_response = requests.get(f'{external_request}&page={page}')
+    while actual_page <= total_pages and actual_page <= max_pages:
+        external_response = requests.get(f'{external_request}&page={actual_page}')
         external_response_status = external_response.status_code
 
-        if page == 1:
+        if actual_page == 1:
+            total_pages = external_response.json().get('total_pages', 0) + 1
             response['status'] = external_response_status
             if external_response_status == 200:
                 response['credentials'] = external_response.json()
@@ -175,12 +153,10 @@ def call_api_multiple_times(external_request, categories):
         else:
             if external_response_status == 200:
                 data = (*data, *external_response.json()['results'])
+        actual_page += 1
 
     response['credentials']['results'] = (*data, *response['credentials']['results'])
-    posters_links, movie_ids = create_array_from_posters_link(response['credentials']['results'])
-    results = detect_main(posters_links, movie_ids, categories.split(','))
-    response['credentials'] = json.loads(results)
-    return Response(response)
+    return response
 
 
 def create_array_from_posters_link(data):
@@ -194,8 +170,31 @@ def create_array_from_posters_link(data):
     return posters_links, movie_ids
 
 
-def save_to_txt(data):
-    posters_link = create_array_from_posters_link(data)
+def create_array_from_trailer_link(movie_ids):
+    start_path = 'https://youtu.be/'
+    trailer_list = []
 
-    with open('posters.txt', 'w') as f:
-        f.write('\n'.join(posters_link))
+    for movie_id in movie_ids:
+        video_response = requests.get(
+            f'https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={API_KEY_TMDB}')
+        videos = [video for video in video_response.json().get("results", []) if video.get("site") == "YouTube"]
+
+        if not videos:
+            continue
+
+        trailer_video = None
+        for video in videos:
+            if video.get("name") == "Official Trailer":
+                trailer_video = video
+                break
+            elif video.get("official") == "true" and not trailer_video:
+                trailer_video = video
+            elif not trailer_video:
+                trailer_video = video
+
+        trailer_list.append({
+            "id": movie_id,
+            "link": start_path + trailer_video["key"]
+        })
+
+    return trailer_list
